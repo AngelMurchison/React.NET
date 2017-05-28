1f339ef2f931c113f77f097a204a2adf3c4cecfd
Angel Murchison <angelmurchison09@gmail.com>
2017-05-28 17:39:17 -0400

    init

---
+++ React.NET01/wwwroot/js/tutorial.jsx
@@ -0,0 +1,65 @@
+﻿var data = [
+    { id: 1, author: "Daniel Lo Nigro", text: "Hello ReactJS.NET World!" },
+    { id: 2, author: "Pete Hunt", text: "This is one comment" },
+    { id: 3, author: "Jordan Walke", text: "This is *another* comment" }
+];
+
+// Add notation for this code and then learn to use this json data blob.
+
+var Comment = React.createClass({
+    rawMarkup: function () {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    },
+
+    render: function () {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
+});
+
+var CommentBox = React.createClass({
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function () {
+        return (
+            <div className="commentList">
+                <Comment author="Daniel Lo Nigro"> Hello ReactJS.NET World!</Comment>
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">This is *another* comment</Comment>
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function () {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm
+            </div>
+        );
+    }
+})
+
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
